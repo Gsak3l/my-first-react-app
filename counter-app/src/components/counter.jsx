aefad67d4dea9ab0b93b5a980b1b2d8f040a48c8
s4kis <gsakel5@gmail.com>
2019-09-21 17:14:51 +0300

    rendering classes dynamically chapter

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -7,19 +7,12 @@ class Counter extends Component
         count:0, 
     };
 
-    styles = 
-    {
-        fontSize: 20,
-        fontWeight: "bold"
-
-    };
-
     render() 
     { 
         return (
             //if we don't want to create a div, we can always use the <React.Fragment></React.Fragment>
             <div>
-                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Incerment</button>
             </div> 
         );
