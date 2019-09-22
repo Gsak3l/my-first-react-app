f782276d6891d1a8edacde38894bac66c71589ea
s4kis <gsakel5@gmail.com>
2019-09-22 19:27:24 +0300

    conditional rendering

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,8 +5,14 @@ class Counter extends Component
     state = 
     {
         count:1, 
-        tags: ['tag1', 'tag2', 'tag3']
+        tags: ['mikey']
     };
+
+    renderTags()
+    {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
 
     render() 
     { 
@@ -14,25 +20,24 @@ class Counter extends Component
             javascript thinks that the keyword return is equal to return ;*/
             //if we don't want to create a div, we can always use the <React.Fragment></React.Fragment>
             <div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Incerment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                {this.state.tags.length === 0 && 'Please create a new tag!'}
+                {this.renderTags()}
             </div> 
+            //true + string = string !! e.x. true && mike = mike
         );
     }
-
+    /*
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes;
     }
-
+    
     formatCount()
     {
         const {count} = this.state;
         return count === 0 ? 'Zero' : count;
     }
+    */
 }
 export default Counter;
