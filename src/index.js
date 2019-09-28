dcda7db9c5715a92658b5bb454318853788ec2cd
s4kis <gsakel5@gmail.com>
2019-09-28 19:33:48 +0300

    finished tutorial

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css'
 
-const element = <h1>Hello world</h1>
-ReactDom.render(element, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
+serviceWorker.unregister();
