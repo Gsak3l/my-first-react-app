5b74a8d1bfb7f68e17d0f35aefb3c60eeaef63a0
s4kis <gsakel5@gmail.com>
2019-09-30 12:35:33 +0300

    Updates

---
+++ precache-manifest.dc574bcccdfd464c50c7eeda55ed216e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7139666f95bd86ec516764ced3ba1642",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "1a352c7a33015f2e39d7",
+    "url": "/my-first-react-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9e400b89b225517b4c4e",
+    "url": "/my-first-react-app/static/css/main.bbc8fbd7.chunk.css"
+  },
+  {
+    "revision": "1a352c7a33015f2e39d7",
+    "url": "/my-first-react-app/static/js/2.eacf3c51.chunk.js"
+  },
+  {
+    "revision": "9e400b89b225517b4c4e",
+    "url": "/my-first-react-app/static/js/main.5f4d5509.chunk.js"
+  },
+  {
+    "revision": "471e80de0004593ffcd6",
+    "url": "/my-first-react-app/static/js/runtime-main.cd74c52f.js"
+  }
+]);
